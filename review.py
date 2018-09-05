327072a5f3f80a59c55f7fe50ff85b9cfb4c2ea8
Pluto <plutoef@gmail.com>
2018-09-05 18:07:12 +0800

    filter good in string

--- review.py
+++ review.py
@@ -21,4 +21,10 @@ for d in data:
 	if len(d) < 100:
 		new.append(d)
 print('一共有', len(new), '筆資料留言小於100')
-print(new[0])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
